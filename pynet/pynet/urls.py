757cf034ef390daca868ade2c652a332a7b6ffde
Vidak Kazic <vidak.kazic@symphony.is>
2017-11-26 13:33:58 +0100

    add jwt

--- pynet/pynet/urls.py
+++ pynet/pynet/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('netapi.urls'))
+    url(r'^', include('netapi.urls')),
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
